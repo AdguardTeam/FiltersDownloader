10d4fd5dac0f0cae50fc32fd85ab348fa7abb121
mizzick <ds.kolyshev@gmail.com>
2018-04-04 23:42:06 +0700

    review fixes

--- gulp-tasks/uglify.js
+++ gulp-tasks/uglify.js
@@ -7,6 +7,8 @@ const gulp = require('gulp');
 const minify = require('gulp-minify');
 
 module.exports = () => {
+    const options = global.options || {};
+
     return gulp.src(path.join(options.outputPath, options.fileName))
         .pipe(minify({
             mangle: false,
