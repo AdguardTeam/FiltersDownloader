462a8b753da3429665960968cf79084ccc0caed5
Maxim Topciu <maximtop@gmail.com>
2020-09-14 09:28:04 +0300

    AG-4057 remove gulpfile

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const concatFiles = require('./gulp-tasks/concat');
-const clean = require('./gulp-tasks/clean');
-const addHeader = require('./gulp-tasks/add-header');
-const uglify = require('./gulp-tasks/uglify');
-
-exports.dev = gulp.series(clean, concatFiles);
-exports.prod = gulp.series(clean, concatFiles, addHeader, uglify);
